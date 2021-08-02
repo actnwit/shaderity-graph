168adbe3ef41e7fe97b29131ca7a81df7db92c91
KaYaF <d.guitar.c@gmail.com>
2021-08-02 20:54:45 +0900

    feat: import ComponentType and CompositionType from RhodoniteTS

---
+++ src/types/AbstractEnumClass.ts
@@ -0,0 +1,103 @@
+// This code idea is from https://qiita.com/junkjunctions/items/5a6d8bed8df8eb3acceb
+
+export interface EnumIO {
+  readonly index: number;
+  readonly str: string;
+  readonly symbol: symbol;
+  toString(): string;
+}
+
+export abstract class AbstractEnumClass implements EnumIO {
+  readonly index: number;
+  readonly symbol: symbol;
+  readonly str: string;
+  private static __indices: Map<Function, number[]> = new Map();
+  private static __strings: Map<Function, string[]> = new Map();
+
+  constructor({
+    index,
+    str,
+    noCheckStrUnique,
+  }: {
+    index: number;
+    str: string;
+    noCheckStrUnique?: boolean;
+  }) {
+    if (AbstractEnumClass.__indices.get(this.constructor) == null) {
+      AbstractEnumClass.__indices.set(this.constructor, []);
+    }
+    if (AbstractEnumClass.__strings.get(this.constructor) == null) {
+      AbstractEnumClass.__strings.set(this.constructor, []);
+    }
+    if (
+      AbstractEnumClass.__indices.get(this.constructor)!.indexOf(index) !== -1
+    ) {
+      throw new Error('Dont use duplicate index.');
+    }
+    if (noCheckStrUnique !== true) {
+      if (
+        AbstractEnumClass.__strings.get(this.constructor)!.indexOf(str) !== -1
+      ) {
+        throw new Error('Dont use duplicate str.');
+      }
+    }
+
+    this.index = index;
+    this.str = str;
+    this.symbol = Symbol(str);
+
+    AbstractEnumClass.__indices.get(this.constructor)!.push(index);
+    AbstractEnumClass.__strings.get(this.constructor)!.push(str);
+  }
+
+  toString(): string {
+    return this.str;
+  }
+}
+
+export function _from({
+  typeList,
+  index,
+}: {
+  typeList: Array<EnumIO>;
+  index: number;
+}): EnumIO | undefined {
+  const match = typeList.find(type => type.index === index);
+  if (!match) {
+    return void 0;
+  }
+
+  return match;
+}
+
+export function _fromString({
+  typeList,
+  str,
+}: {
+  typeList: Array<EnumIO>;
+  str: string;
+}): EnumIO | undefined {
+  const match = typeList.find(
+    type => type.str.toLowerCase() === str.toLowerCase()
+  );
+  if (!match) {
+    return void 0;
+  }
+
+  return match;
+}
+
+export function _fromStringCaseSensitively({
+  typeList,
+  str,
+}: {
+  typeList: Array<EnumIO>;
+  str: string;
+}): EnumIO | undefined {
+  const match = typeList.find(type => type.str === str);
+  if (!match) {
+    return void 0;
+  }
+
+  return match;
+}
