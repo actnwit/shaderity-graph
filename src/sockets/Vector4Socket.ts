d3951d53ee220302273da961fea68aa2e4f9b463
KaYaF <d.guitar.c@gmail.com>
2021-08-02 18:34:45 +0900

    feat: add Vector4 socket

---
+++ src/sockets/Vector4Socket.ts
@@ -0,0 +1,11 @@
+import {NodeId} from '../types/CommonType';
+import AbstractSocket, {
+  SocketDirectionEnum,
+  SocketType,
+} from './AbstractSocket';
+
+export default class Vector4Socket extends AbstractSocket {
+  constructor(socketDirection: SocketDirectionEnum, nodeID: NodeId) {
+    super('vector4_' + nodeID, SocketType.Vector4, socketDirection, nodeID);
+  }
+}
