a3d11d71e3c8cbebfc005bcf74590e56c5c7800c
KaYaF <d.guitar.c@gmail.com>
2021-08-06 20:25:33 +0900

    feat: import nodes from json

--- src/import/JsonImporter.ts
+++ src/import/JsonImporter.ts
@@ -1,7 +1,17 @@
+import Node from '../nodes/Node';
 import {ShaderityGraphNodeJson} from '../types/CommonType';
 
 export default class JsonImporter {
-  public static importJsonToNodes(nodesJson: ShaderityGraphNodeJson[]) {
-    console.log(nodesJson);
+  static importJsonToNodes(nodesJson: ShaderityGraphNodeJson[]) {
+    for (let i = 0; i < nodesJson.length; i++) {
+      const nodeJson = nodesJson[i];
+
+      // Node.__nodeId equals to index of the nodesJson array
+      new Node(
+        nodeJson.shaderityData.shaderStage,
+        nodeJson.shaderityData.shaderCode,
+        nodeJson.shaderityData.nodeName
+      );
+    }
   }
 }
