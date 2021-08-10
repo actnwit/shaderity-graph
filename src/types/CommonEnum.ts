c4ed2ffc36cfd370136725bdfc17becc4d43bcf7
KaYaF <d.guitar.c@gmail.com>
2021-08-10 19:34:32 +0900

    feat: add some socket types

--- src/types/CommonEnum.ts
+++ src/types/CommonEnum.ts
@@ -7,14 +7,21 @@ export const ShaderStage = {
 export type ShaderStageEnum = typeof ShaderStage[keyof typeof ShaderStage];
 
 const _SocketType = {
+  Bool: 'bool',
   Int: 'int',
   Float: 'float',
   Vec2: 'vec2',
   Vec3: 'vec3',
   Vec4: 'vec4',
+  IVec2: 'ivec2',
+  IVec3: 'ivec3',
+  IVec4: 'ivec4',
   Mat22: 'mat22',
   Mat33: 'mat33',
   Mat44: 'mat44',
+  Texture2D: 'sampler2D',
+  // Texture3D: 'sampler3D',
+  TextureCube: 'samplerCube',
 } as const;
 
 export const SocketType = {
