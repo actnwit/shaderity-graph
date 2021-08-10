fd14a4b993fa7a1d31208bcdf9208ae58817f66f
KaYaF <d.guitar.c@gmail.com>
2021-08-10 13:21:33 +0900

    feat: add __findBeginNode method to sort nodes topologically

--- src/node/NodeSorter.ts
+++ src/node/NodeSorter.ts
@@ -2,6 +2,25 @@ import Node from '../node/Node';
 
 export default class NodeSorter {
   static sortTopologically(nodes: Node[]): Node[] {
+    const beginNode = this.__findBeginNode(nodes);
     return [];
+  }
+
+  private static __findBeginNode(nodes: Node[]): Node {
+    let noInputSocketNode: Node | undefined;
+    for (const node of nodes) {
+      const sockets = node.inputSockets;
+      const inputSocketCount = Object.keys(sockets).length;
+      if (inputSocketCount === 0) {
+        noInputSocketNode = node;
+        break;
+      }
+    }
+
+    if (noInputSocketNode == null) {
+      throw Error('NodeSorter:__findBeginNode: begin node not found');
+    }
+
+    return noInputSocketNode;
   }
 }
