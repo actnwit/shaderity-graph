bf1abb440c495f24a62952e8ed3f1c21d8c65084
KaYaF <d.guitar.c@gmail.com>
2021-09-13 17:43:31 +0900

    refactor: update ShaderityGraphJson to use ShaderityObjectCreator

--- src/types/CommonType.ts
+++ src/types/CommonType.ts
@@ -1,31 +1,85 @@
+import {
+  ShaderPrecisionType as _ShaderPrecisionType,
+  ShaderAttributeVarType as _ShaderAttributeVarType,
+  ShaderConstantValueVarTypeES3 as _ShaderConstantValueVarTypeES3,
+  ShaderVaryingVarType as _ShaderVaryingVarType,
+  ShaderVaryingInterpolationType as _ShaderVaryingInterpolationType,
+  ShaderPrecisionObject as _ShaderPrecisionObject,
+  ShaderAttributeObject as _ShaderAttributeObject,
+  ShaderConstantValueObject as _ShaderConstantValueObject,
+  ShaderUniformObject as _ShaderUniformObject,
+  ShaderVaryingObject as _ShaderVaryingObject,
+} from 'shaderity/dist/esm';
 import {SocketTypeEnum} from './CommonEnum';
+
+export type ShaderPrecisionType = _ShaderPrecisionType;
+export type ShaderAttributeVarType = _ShaderAttributeVarType;
+export type ShaderConstantValueVarTypeES3 = _ShaderConstantValueVarTypeES3;
+export type ShaderVaryingVarType = _ShaderVaryingVarType; // equals to _ShaderAttributeVarType
+export type ShaderVaryingInterpolationType = _ShaderVaryingInterpolationType;
+export type ShaderPrecisionObject = _ShaderPrecisionObject;
+export type ShaderAttributeObject = _ShaderAttributeObject;
+export type ShaderConstantValueObject = _ShaderConstantValueObject;
+export type ShaderUniformObject = _ShaderUniformObject;
+export type ShaderVaryingObject = _ShaderVaryingObject;
 
 export type NodeId = number;
 
-export type ShaderCodes = {
+export interface ShaderCodes {
   vertexShaderCode: string;
   pixelShaderCode: string;
-};
+}
 
-export type ShaderityGraphJson = {
+export interface ShaderityGraphJson {
   version: string;
   shaderName: string;
   nodes: ShaderityGraphNode[];
-};
+  vertexShaderGlobalData?: ShaderGlobalData;
+  fragmentShaderGlobalData?: ShaderGlobalData;
+}
 
-export type ShaderityGraphNode = {
-  inputNodes: {[key: string]: ConnectedNode};
-  outputNodes: {[key: string]: ConnectedNode};
-  nodeData: {
-    shaderFunctionName: string;
-    shaderFunctionCode: string;
-    shaderStage: 'vertex' | 'pixel' | 'noUse';
-    extras: {[key: string]: unknown};
-  };
-  extras: {[key: string]: unknown};
-};
+export interface ShaderityGraphNode {
+  nodeData:
+    | NodeData
+    | AttributeInputNodeData
+    | VaryingInputNodeData
+    | UniformInputNodeData;
+  inputNodes?: {[key: string]: ConnectedInputNode};
+  outputNodes?: {[key: string]: ConnectedNode};
+  extras?: {[key: string]: unknown};
+}
 
-export type ConnectedNode = {
+export interface NodeData {
+  shaderFunctionName: string;
+  shaderFunctionCode: string;
+  shaderStage: 'vertex' | 'pixel' | 'noUse';
+  extensions?: string[];
+  extras?: {[key: string]: unknown};
+}
+
+export interface AttributeInputNodeData extends NodeData {
+  attribute: ShaderAttributeObject;
+}
+
+export interface VaryingInputNodeData extends NodeData {
+  varying: ShaderVaryingObject;
+}
+
+export interface UniformInputNodeData extends NodeData {
+  uniform: ShaderUniformObject;
+}
+
+export interface ShaderGlobalData {
+  defineDirectives?: string[];
+  precision?: ShaderPrecisionObject;
+  constantValues?: ShaderConstantValueObject[];
+}
+
+export interface ConnectedNode {
   nodeId: number;
   socketType: SocketTypeEnum;
-};
+}
+
+export interface ConnectedInputNode extends ConnectedNode {
+  defaultValue: number[];
+}
