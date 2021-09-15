1ec5bf8db54f04d7129117b6a8d6795f168377d6
KaYaF <d.guitar.c@gmail.com>
2021-09-15 11:23:03 +0900

    refactor: separate uniform input node from node

---
+++ src/node/UniformInputNode.ts
@@ -0,0 +1,27 @@
+import {UniformInputNodeData} from '../types/CommonType';
+import Node, {NodeClassNames} from './Node';
+
+export default class UniformInputNode extends Node {
+  protected __nodeData: UniformInputNodeData;
+
+  constructor(nodeData: UniformInputNodeData) {
+    super(nodeData);
+    this.__nodeData = nodeData;
+  }
+
+  get className(): NodeClassNames {
+    return 'UniformInputNode';
+  }
+
+  get variableName() {
+    return this.__nodeData.uniform.variableName;
+  }
+
+  get type() {
+    return this.__nodeData.uniform.type;
+  }
+
+  get precision() {
+    return this.__nodeData.uniform.precision;
+  }
+}
