dd7c201b01f34c4a073f0c9d57bb02cfaea8751a
KaYaF <d.guitar.c@gmail.com>
2021-09-22 19:14:41 +0900

    refactor: remove needless part

--- src/system/NodeSorter.ts
+++ src/system/NodeSorter.ts
@@ -1,35 +1,7 @@
 import Node from '../node/Node';
 
 export default class NodeSorter {
-  static sortTopologically(nodes: Node[]): Node[] {
-    if (nodes.length === 0) {
-      return nodes;
-    }
-
-    const beginNode = this.__findBeginNode(nodes);
-    const sortedNodes = this.__sortTopologically(beginNode, nodes);
-    return sortedNodes;
-  }
-
-  private static __findBeginNode(nodes: Node[]): Node {
-    let noInputSocketNode: Node | undefined;
-    for (const node of nodes) {
-      const sockets = node._inputSockets;
-      const inputSocketCount = sockets.length;
-      if (inputSocketCount === 0) {
-        noInputSocketNode = node;
-        break;
-      }
-    }
-
-    if (noInputSocketNode == null) {
-      throw Error('NodeSorter:__findBeginNode: begin node not found');
-    }
-
-    return noInputSocketNode;
-  }
-
-  private static __sortTopologically(beginNode: Node, unsortedNodes: Node[]) {
+  static sortTopologically(unsortedNodes: Node[]): Node[] {
     // this sort is based on https://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/
 
     // Storage of edges (e.g. edge[0] = [1] means unsortedNodes[0] has the edge to unsortedNodes[1])
