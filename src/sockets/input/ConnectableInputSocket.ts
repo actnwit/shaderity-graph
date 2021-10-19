6c85e4c030eab637d3c17fdd9851e4aa0a3ec9a6
KaYaF <d.guitar.c@gmail.com>
2021-10-19 17:36:01 +0900

    docs: update document of Sockets

--- src/sockets/input/ConnectableInputSocket.ts
+++ src/sockets/input/ConnectableInputSocket.ts
@@ -5,13 +5,21 @@ import {IConnectableOutputSocket} from '../output/IConnectableOutputSocket';
 import {INode} from '../../node/INode';
 import AbstractConnectableSocket from '../AbstractConnectableSocket';
 
-// An InputSocket can connect only one OutputSocket
+/**
+ * The ConnectableInputSocket is an input socket that can connect to a ConnectableOutputSocket.
+ * This socket can connect to at most one ConnectableOutputSocket.
+ */
 export default class ConnectableInputSocket
   extends AbstractConnectableSocket
   implements IConnectableInputSocket
 {
   _connectedSocket: IConnectableOutputSocket | undefined = undefined;
 
+  /**
+   * @private
+   * If this socket does not connect with output socket,
+   * the __defaultValue is used as input value.
+   */
   private __defaultValue: number[];
 
   constructor(
@@ -24,18 +32,32 @@ export default class ConnectableInputSocket
     this.__defaultValue = defaultValue;
   }
 
+  /**
+   * get the defaultValue of this socket
+   */
   get defaultValue() {
     return this.__defaultValue;
   }
 
+  /**
+   * Get the class name of this socket
+   */
   get className(): SocketClassName {
     return 'ConnectableInputSocket';
   }
 
+  /**
+   * Get the node that has the socket connected to this socket
+   * @returns connected node or undefined
+   */
   get connectedNode() {
     return this._connectedSocket?.node;
   }
 
+  /**
+   * Get the socket that is connected to this socket
+   * @returns connected socket or undefined
+   */
   get connectedSocket() {
     return this._connectedSocket;
   }
