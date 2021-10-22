2f0d42c41d54737d05b74f98167d467923a8c30e
KaYaF <d.guitar.c@gmail.com>
2021-10-22 11:18:53 +0900

    feat: introduce interface of varying input/output socket

---
+++ src/sockets/input/IVaryingInputSocket.ts
@@ -0,0 +1,22 @@
+import {INode} from '../../node/INode';
+import {
+  ShaderPrecisionType,
+  ShaderVaryingInterpolationType,
+  ShaderVaryingVarType,
+} from '../../types/CommonType';
+import {ISocket} from '../ISocket';
+import {IVaryingOutputSocket} from '../output/IVaryingOutputSocket';
+
+export interface IVaryingInputSocket extends ISocket {
+  _connectedSocket: IVaryingOutputSocket | undefined;
+
+  get className(): 'VaryingInputSocket';
+  get variableName(): string;
+  get socketType(): ShaderVaryingVarType;
+  get precision(): ShaderPrecisionType;
+  get interpolationType(): ShaderVaryingInterpolationType | undefined;
+  get connectedNode(): INode | undefined;
+  get connectedSocket(): IVaryingOutputSocket | undefined;
+
+  _connectSocketWith(socket: ISocket): void;
+}
